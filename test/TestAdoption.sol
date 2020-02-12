c98f9009506be9760c168670a1fe982f2dc00864
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2020-02-12 23:21:41 +0000

    Test the adoption smart contract
    * Run `truffle test` to execute tests

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-import "truffle/Asset.sol";
+import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/Adoption.sol";
 
@@ -11,6 +11,28 @@ contract TestAdoption {
     // The id of the pet that will be used for testing
     uint expectedPetId = 8;
 
-    // The expected owner of the adopted pet is this contract
-    address expectedOwner = address(this);
+    // The expected adopter of the adopted pet is this contract
+    address expectedAdopter = address(this);
+
+    // Testing the adopt() function
+    function testUserCanAdoptPet() public {
+        uint returnedId = adoption.adopt(expectedPetId);
+
+        Assert.equal(returnedId, expectedPetId, "Adoption of the expected pet should match what is returned.");
+    }
+
+    // Testing the retrieval of a single pet's owner using the automatic getter on a public variable
+    function testGetAdopterAddressByPetId() public {
+        address adopter = adoption.adopters(expectedPetId);
+
+        Assert.equal(adopter, expectedAdopter, "Owner of the expected pet should be this contract");
+    }
+
+    // Testing retrieval of all pet owners
+    function testGetAdopterAddressByPetIdInArray() public {
+        // Store adopters in memory rather than contract's storage
+        address[16] memory adopters = adoption.getAdopters();
+
+        Assert.equal(adopters[expectedPetId], expectedAdopter, "Owner of the expected pet should be this contract");
+    }
 }
